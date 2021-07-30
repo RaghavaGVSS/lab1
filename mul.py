c4144ce17448daa42b35be21ad144013713b2fac
RaghavaGVSS <190030489@kluniversity.in>
2021-07-30 18:08:19 +0530

    second commit

---
+++ mul.py
@@ -0,0 +1,3 @@
+a=int(input("Enter a:"))
+b=int(input("Enter b:"))
+print("Mul: ",a*b)
---
+++ sum.java
@@ -0,0 +1,12 @@
+//This is sum.java from gitbash
+package pack;
+import java.util.Scanner;
+private class DevOps{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter 2 nums:");
+int a,b;a=sc.nextInt();
+b=sc.nextInt();
+System.out.println("Sum: "+(a+b));
+}
+}
